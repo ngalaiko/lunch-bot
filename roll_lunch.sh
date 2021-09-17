73f84815daccd98c31b38e7350537c8522b3ef64
Nikita Galaiko <nikita@galaiko.rocks>
2021-09-17 09:11:09 +0000

    make the message more fun
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 419e43dd-93af-41ce-a1aa-5b7ba68cd512

--- roll_lunch.sh
+++ roll_lunch.sh
@@ -4,5 +4,5 @@ lunch_place=$(sort -R ./lunch | head -1)
 
 curl -X POST \
     -H 'Content-type: application/json' \
-    --data "{\"text\": \"Today's lunch place is ${lunch_place}\"}" \
+    --data "{\"text\": \"Today's lunch place is... ${lunch_place}!\"}" \
     "https://hooks.slack.com/services/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"
