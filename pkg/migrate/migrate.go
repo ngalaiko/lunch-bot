e5220435a88cea605cee5f0ffe7ec38d57741dec
Nikita Galaiko <nikita@galaiko.rocks>
2021-11-20 14:04:10 +0000

    remove s3
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 337f0cd4-6f8f-4007-bb5f-346f579327c7

--- pkg/migrate/migrate.go
+++ pkg/migrate/migrate.go
@@ -3,11 +3,7 @@ package migrate
 import (
 	"context"
 	"fmt"
-	"log"
 
-	storage_boosts "lunch/pkg/lunch/boosts/storage"
-	storage_places "lunch/pkg/lunch/places/storage"
-	storage_rolls "lunch/pkg/lunch/rolls/storage"
 	"lunch/pkg/store"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -24,25 +20,9 @@ func mustLoadConfig() aws.Config {
 
 var (
 	cfg           = mustLoadConfig()
-	s3Store       = store.NewS3(cfg)
 	dynamodbStore = store.NewDynamoDB(cfg)
 )
 
 func Run(ctx context.Context) error {
-	log.Printf("[INFO] migrating places")
-	if err := migratePlaces(ctx, storage_places.NewS3(s3Store), storage_places.NewDynamoDB(dynamodbStore)); err != nil {
-		return fmt.Errorf("failed to migrate places: %w", err)
-	}
-
-	log.Printf("[INFO] migrating boosts")
-	if err := migrateBoosts(ctx, storage_boosts.NewS3(s3Store), storage_boosts.NewDynamoDB(dynamodbStore)); err != nil {
-		return fmt.Errorf("failed to migrate boosts: %w", err)
-	}
-
-	log.Printf("[INFO] migrating rolls")
-	if err := migrateRolls(ctx, storage_rolls.NewS3(s3Store), storage_rolls.NewDynamoDB(dynamodbStore)); err != nil {
-		return fmt.Errorf("failed to migrate boosts: %w", err)
-	}
-
-	return nil
+	return fmt.Errorf("nothing to do")
 }
