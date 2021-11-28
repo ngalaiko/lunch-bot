17a697a4500f23312112f393e454225e2b14dfd0
Nikita Galaiko <nikita@galaiko.rocks>
2021-11-28 12:08:00 +0000

    add slack secret
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 337f0cd4-6f8f-4007-bb5f-346f579327c7

--- pkg/http/slack/request.go
+++ pkg/http/slack/request.go
@@ -10,6 +10,8 @@ import (
 	"github.com/hetiansu5/urlquery"
 )
 
+var signingSecret = "notset"
+
 type FormURLEncodedRequest struct {
 	Command  string `query:"command"`
 	Text     string `query:"text"`
