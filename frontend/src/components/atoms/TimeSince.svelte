e742077dc5040fb54cde7f9dd3bc48ff6db94e1f
Nikita Galaiko <nikita@galaiko.rocks>
2022-01-14 08:47:07 +0000

    frontend: fix time since
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 337f0cd4-6f8f-4007-bb5f-346f579327c7

--- frontend/src/components/atoms/TimeSince.svelte
+++ frontend/src/components/atoms/TimeSince.svelte
@@ -3,10 +3,11 @@
   export let to: Date
 
   const humanDiff = (from: Date, to: Date): string => {
-    const diff = to.getTime() - from.getTime()
-    const seconds = Math.floor(diff / 1000)
-    const minutes = Math.floor(diff / 1000 / 60)
-    const days = Math.floor(minutes / 60 / 24)
+    const milliseconds = to.getTime() - from.getTime()
+    const seconds = Math.floor(milliseconds / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
     const weeks = Math.floor(days / 7)
     const months = Math.floor(days / 30)
     const years = Math.floor(days / 365)
@@ -19,6 +20,8 @@
       return weeks === 1 ? 'last week' : `${weeks} weeks ago`
     } else if (days > 0) {
       return days === 1 ? 'yesterday' : `${days} days ago`
+    } else if (hours > 0) {
+      return hours === 1 ? 'last hour' : `${hours} hours ago`
     } else if (minutes > 0) {
       return minutes === 1 ? 'a minute ago' : `${minutes} minutes ago`
     } else if (seconds > 0) {
