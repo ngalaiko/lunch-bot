acf2709ad5c8962120ac8e5abb7e57e894910b56
Nikita Galaiko <nikita@galaiko.rocks>
2022-01-21 07:52:05 +0000

    fix bolt implementation
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 337f0cd4-6f8f-4007-bb5f-346f579327c7

--- backend/pkg/lunch/rolls/storage/bolt.go
+++ backend/pkg/lunch/rolls/storage/bolt.go
@@ -24,10 +24,14 @@ func (s *BoltDBStorage) Store(ctx context.Context, boost *rolls.Roll) error {
 	return s.db.Put(ctx, s.bucketName, string(boost.ID), boost)
 }
 
-func (s *BoltDBStorage) ListRolls(ctx context.Context) ([]*rolls.Roll, error) {
-	var rolls = []*rolls.Roll{}
-	if _, err := s.db.List(ctx, s.bucketName, &rolls, 100, nil); err != nil {
+func (s *BoltDBStorage) ListRolls(ctx context.Context) (map[rolls.ID]*rolls.Roll, error) {
+	var rr = []*rolls.Roll{}
+	if _, err := s.db.List(ctx, s.bucketName, &rr, 100, nil); err != nil {
 		return nil, fmt.Errorf("failed to list rolls: %w", err)
 	}
-	return rolls, nil
+	m := make(map[rolls.ID]*rolls.Roll, len(rr))
+	for _, r := range rr {
+		m[r.ID] = r
+	}
+	return m, nil
 }
