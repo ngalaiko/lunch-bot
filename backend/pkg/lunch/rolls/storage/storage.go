76262b0ec252812220bc431212543b8097a1bf68
Nikita Galaiko <nikita@galaiko.rocks>
2022-02-07 16:44:18 +0000

    backend: rm v1 storage
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2742f133-922b-4ce3-9dfe-eb2554c18bc1

--- backend/pkg/lunch/rolls/storage/storage.go
+++ backend/pkg/lunch/rolls/storage/storage.go
@@ -2,11 +2,50 @@ package storage
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"lunch/pkg/lunch/events"
 	"lunch/pkg/lunch/rolls"
+	"lunch/pkg/lunch/rooms"
 )
 
-type Storage interface {
-	Store(context.Context, *rolls.Roll) error
-	ListRolls(context.Context) (map[rolls.ID]*rolls.Roll, error)
+var (
+	rollCreated events.Type = "rolls/created"
+)
+
+type Storage struct {
+	eventsStorage events.Storage
+}
+
+func New(eventsStorage events.Storage) *Storage {
+	return &Storage{
+		eventsStorage: eventsStorage,
+	}
+}
+
+func (s *Storage) Create(ctx context.Context, roll *rolls.Roll) error {
+	return s.eventsStorage.Create(ctx, &events.Event{
+		UserID:    roll.UserID,
+		PlaceID:   roll.PlaceID,
+		RoomID:    roll.RoomID,
+		Type:      rollCreated,
+		Timestamp: events.UnixNanoTime(roll.Time),
+	})
+}
+
+func (s *Storage) Rolls(ctx context.Context, roomID rooms.ID) ([]*rolls.Roll, error) {
+	events, err := s.eventsStorage.ByRoomID(ctx, roomID, rollCreated)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get events: %w", err)
+	}
+	result := make([]*rolls.Roll, 0, len(events))
+	for _, event := range events {
+		result = append(result, &rolls.Roll{
+			UserID:  event.UserID,
+			PlaceID: event.PlaceID,
+			Time:    time.Time(event.Timestamp),
+		})
+	}
+	return result, nil
 }
--- backend/pkg/migrate/migrate.go
+++ backend/pkg/migrate/migrate.go
@@ -2,15 +2,11 @@ package migrate
 
 import (
 	"context"
-	"fmt"
 
 	storage_boosts "lunch/pkg/lunch/boosts/storage"
-	storage_boosts_v2 "lunch/pkg/lunch/boosts/storage/v2"
 	"lunch/pkg/lunch/events"
 	storage_places "lunch/pkg/lunch/places/storage"
-	storage_places_v2 "lunch/pkg/lunch/places/storage/v2"
 	storage_rolls "lunch/pkg/lunch/rolls/storage"
-	storage_rolls_v2 "lunch/pkg/lunch/rolls/storage/v2"
 	"lunch/pkg/lunch/rooms"
 	"lunch/pkg/store"
 
@@ -30,52 +26,14 @@ var (
 	cfg           = mustLoadConfig()
 	dynamodbStore = store.NewDynamoDB(cfg)
 
-	placesv1 = storage_places.NewDynamoDB(dynamodbStore, "lunch-production-webapp-places")
-	boostsv1 = storage_boosts.NewDynamoDB(dynamodbStore, "lunch-production-webapp-boosts")
-	rollsv1  = storage_rolls.NewDynamoDB(dynamodbStore, "lunch-production-webapp-rolls")
-
 	eventsStore = events.NewDynamoDBStore(dynamodbStore, "lunch-production-webapp-events")
-	placesv2    = storage_places_v2.New(eventsStore)
-	boostsv2    = storage_boosts_v2.New(eventsStore)
-	rollsv2     = storage_rolls_v2.New(eventsStore)
+	places      = storage_places.New(eventsStore)
+	boosts      = storage_boosts.New(eventsStore)
+	rolls       = storage_rolls.New(eventsStore)
 
 	sturdyRoomID = rooms.ID("69c83096-995a-48ce-b843-80a926b0a9ec")
 )
 
 func Run(ctx context.Context) error {
-	places, err := placesv1.ListAll(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to load places: %w", err)
-	}
-	for _, place := range places {
-		place.RoomID = sturdyRoomID
-
-		fmt.Printf("place.ID: %v %d\n", place.ID, place.Time.UnixNano())
-		if err := placesv2.Create(ctx, place); err != nil {
-			return fmt.Errorf("failed to migrate place: %w", err)
-		}
-	}
-
-	boosts, err := boostsv1.ListBoosts(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to load boosts: %w", err)
-	}
-	for _, boost := range boosts {
-		boost.RoomID = sturdyRoomID
-		if err := boostsv2.Create(ctx, boost); err != nil {
-			return fmt.Errorf("failed to migrate boost: %w", err)
-		}
-	}
-
-	rolls, err := rollsv1.ListRolls(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to load rolls: %w", err)
-	}
-	for _, roll := range rolls {
-		roll.RoomID = sturdyRoomID
-		if err := rollsv2.Create(ctx, roll); err != nil {
-			return fmt.Errorf("failed to migrate roll: %w", err)
-		}
-	}
 	return nil
 }
